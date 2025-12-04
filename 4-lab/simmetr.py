3c0d233562da929836e824955f99913301e0df44
qfunky <mrkeld75@gmail.com>
2025-12-04 23:33:32 +0300

    small fix

--- 4-lab/simmetr.py
+++ 4-lab/simmetr.py
@@ -11,7 +11,7 @@ for i in range(n):
     for j in range(n):
         if matrix[i][j] != matrix[j][i]:
             is_symmetric = False
-            break # Если нашли несовпадение, дальше можно не искать
+            break
 
 if is_symmetric:
     print("YES")
